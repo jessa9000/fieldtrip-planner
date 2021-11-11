1752d265d3daa2ad0af300e2106fea9c589b63fc
jessa9000 <ealickj@oregonstate.edu>
2021-11-10 20:58:24 -0500

    Added column aliases

--- Data_manipulation_queries.sql
+++ Data_manipulation_queries.sql
@@ -7,7 +7,9 @@
 
 -- Students table
 
-SELECT * FROM students;
+SELECT studentID AS 'Student ID', firstName AS 'First Name', lastName AS 'Last Name', schoolYear AS 'School Year',
+    allergiesFlag AS 'Any Allergies', specialPower AS 'Special Power' 
+    FROM students;
 
 -- "Add a Student" input form will kick off up to two separate INSERT statements:
 
@@ -21,7 +23,8 @@ INSERT INTO allergies
 
 -- Emergency Contacts table
 
-SELECT * FROM emergencyContacts;
+SELECT studentID AS 'Student ID', adultID AS 'Adult ID' 
+    FROM emergencyContacts;
 
 -- "Add an Emergency Contact" input form
 
@@ -36,7 +39,9 @@ INSERT INTO emergencyContacts
 
 -- Trusted Adults table
 
-SELECT * FROM trustedAdults;
+SELECT adultID AS 'Trusted Adult ID', firstName AS 'First Name', lastName AS 'Last Name', 
+    primaryPhone AS 'Primary Phone' 
+    FROM trustedAdults;
 
 -- "Add a Trusted Adult" input form
 
@@ -50,14 +55,16 @@ INSERT INTO trustedAdults
 
 -- Allergies table with two joins
 
-SELECT allergies.studentID, students.firstName, students.lastName, allergies.allergenID, allergens.name 
+SELECT allergies.studentID AS 'Student ID', students.firstName AS 'Student First Name', 
+    students.lastName AS 'Student Last Name', allergies.allergenID AS 'Allergen ID', allergens.name AS 'Allergen'
     FROM allergies
     JOIN students ON allergies.studentID = students.studentID
     JOIN allergens ON allergies.allergenID = allergens.allergenID;
 
 -- Possible allergens
 
-SELECT * FROM allergens;
+SELECT allergenID AS 'Allergen ID', name AS 'Allergen Name' 
+    FROM allergens;
 
 -- "Add an Allergy" input form
 
@@ -78,7 +85,8 @@ INSERT INTO allergens
 
 -- Snacks table
 
-SELECT * FROM snacks;
+SELECT snackID AS 'Snack ID', name AS 'Snack Name' 
+    FROM snacks;
 
 -- "Add a Snack" input form
 
@@ -93,7 +101,8 @@ DELETE FROM snacks
 
 -- Ingredients table with two joins
 
-SELECT ingredients.snackID, snacks.name, ingredients.allergenID, allergens.name
+SELECT ingredients.snackID AS 'Snack ID', snacks.name AS 'Snack Name', 
+    ingredients.allergenID AS 'Allergen ID', allergens.name AS 'Allergen'
     FROM ingredients
     JOIN snacks ON ingredients.snackID = snacks.snackID
     JOIN allergens ON ingredients.allergenID = allergens.allergenID;
@@ -110,7 +119,9 @@ INSERT INTO ingredients
 
 -- Trips table
 
-SELECT * FROM trips;
+SELECT tripID AS 'Trip ID', name as 'Name', street as 'Street', city AS 'City', state AS 'State',
+    zipCode AS 'Zip Code', date AS 'Date', meetTime AS 'Meet Time', returnTime AS 'Return Time' 
+    FROM trips;
 
 -- "Add a Trip" input form
 
@@ -136,12 +147,17 @@ UPDATE trips
 
 -- "Select a trip to plan" input form and resulting Trip in Planning table
 
-SELECT * FROM trips WHERE tripID = ::tripID OR name LIKE %::keyword%;
+SELECT tripID AS 'Trip ID', name as 'Name', street as 'Street', city AS 'City', state AS 'State',
+    zipCode AS 'Zip Code', date AS 'Date', meetTime AS 'Meet Time', returnTime AS 'Return Time' 
+    FROM trips 
+    WHERE tripID = ::tripID OR name LIKE %::keyword%;
 
 -- Attendees table
 
-SELECT attendees.tripID, attendees.studentID, students.firstName, students.lastName, attendees.adultID, 
-    adults.firstName, adults.lastName
+SELECT attendees.tripID AS 'Trip ID', attendees.studentID AS 'Student ID', 
+    students.firstName AS 'Student First Name', students.lastName AS 'Student Last Name', 
+    attendees.adultID AS 'Responsible Chaperone ID', 
+    adults.firstName AS 'Responsible Chaperone FName', adults.lastName AS 'Responsible Chaperone LName'
     FROM attendees
     JOIN students ON attendees.studentID = students.studentID
     JOIN trustedAdults ON attendees.adultID = trustedAdults.adultID
