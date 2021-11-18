ac8d54a45abba5cea3158e321ab6bc3c54260c05
mingusmingusmingusmingusmingus <59594410+mingusmingusmingusmingusmingus@users.noreply.github.com>
2021-11-18 01:09:43 -0500

    Adding initial routes to template pages

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 import os
+import database.db_connector as db
 
 # Configuration
 
@@ -32,11 +33,79 @@ people_from_app_py = [
 }
 ]
 
+db_connection = db.connect_to_database()
+
 # Routes 
 
 @app.route('/')
 def root():
     return render_template("main.j2", people=people_from_app_py)
+
+
+@app.route('/Students')
+def Students():
+ 
+    # Write the query and save it to a variable
+    query = "SELECT * FROM Students;"
+
+    # The way the interface between MySQL and Flask works is by using an
+    # object called a cursor. Think of it as the object that acts as the
+    # person typing commands directly into the MySQL command line and
+    # reading them back to you when it gets results
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+
+    # The cursor.fetchall() function tells the cursor object to return all
+    # the results from the previously executed
+    #
+    # The json.dumps() function simply converts the dictionary that was
+    # returned by the fetchall() call to JSON so we can display it on the
+    # page.
+    results = cursor.fetchall()
+
+    # Sends the results back to the web browser.
+    return render_template("Students.j2", Students=results)
+
+@app.route('/TrustedAdults')
+def TrustedAdults():
+ 
+    query = "SELECT * FROM TrustedAdults;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("TrustedAdults.j2", TrustedAdults=results)
+
+@app.route('/Allergies')
+def Allergies():
+ 
+    query = "SELECT * FROM Allergies;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("Allergies.j2", Allergies=results)
+
+@app.route('/Snacks')
+def Snacks():
+ 
+    query = "SELECT * FROM Snacks;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("Snacks.j2", Snacks=results)
+
+@app.route('/Trips')
+def Trips():
+ 
+    query = "SELECT * FROM Trips;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("Trips.j2", Trips=results)
+
+"""
+@app.route('/TripPlanner')
+def TripPlanner():
+ 
+    query = "SELECT * FROM TripPlanner;"
+    cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("TripPlanner.j2", TripPlanner=results)
+"""
 
 # Listener
 
