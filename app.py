1f5298cbc9de7070f9cf7cf0a23d2132d9673e61
jessa9000 <ealickj@oregonstate.edu>
2021-11-17 19:21:50 -0500

    Finished with Step 4 of starter app (dummy data included, will delete in the next step)

--- app.py
+++ app.py
@@ -5,11 +5,38 @@ import os
 
 app = Flask(__name__)
 
+people_from_app_py = [
+{
+    "name": "Thomas",
+    "age": 33,
+    "location": "New Mexico",
+    "favorite_color": "Blue"
+},
+{
+    "name": "Gregory",
+    "age": 41,
+    "location": "Texas",
+    "favorite_color": "Red"
+},
+{
+    "name": "Vincent",
+    "age": 27,
+    "location": "Ohio",
+    "favorite_color": "Green"
+},
+{
+    "name": "Alexander",
+    "age": 29,
+    "location": "Florida",
+    "favorite_color": "Orange"
+}
+]
+
 # Routes 
 
 @app.route('/')
 def root():
-    return render_template("main.j2")
+    return render_template("main.j2", people=people_from_app_py)
 
 # Listener
 
