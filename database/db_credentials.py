e9ca848c078ba489578a02643745aef8d3850b1f
jessa9000 <ealickj@oregonstate.edu>
2021-11-18 20:44:58 -0500

    Updated credentials file, not sure about db name?

--- database/db_credentials.py
+++ database/db_credentials.py
@@ -5,15 +5,15 @@
 # You can also use environment variables
 
 # For Local Devlelopment
-host = 'localhost'
-user = 'root'                                   # can be different if you set up another username in your MySQL installation
-passwd = 'nottellingyou'                        # set accordingly
-db = 'bsg'
+# host = 'localhost'
+# user = 'root'                                   # can be different if you set up another username in your MySQL installation
+# passwd = 'nottellingyou'                        # set accordingly
+# db = 'bsg'
 
 
 # For OSU Flip Servers
 
-# host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
-# user = 'cs-340-ealickj'                       # don't forget the CS_340 prefix
-# passwd = '****'                               # should only be 4 digits if default
-# db = '<name-of-database-on-osu-server>'       # Jess here - Where do we name this??                           
+host = 'classmysql.engr.oregonstate.edu'      # MUST BE THIS
+user = 'cs-340-ealickj'                       # don't forget the CS_340 prefix
+passwd = '3912'                               # should only be 4 digits if default
+db = 'fieldtrips'       # Jess here - I just wrote 'fieldtrips' here, but not sure what it maps to??                           
