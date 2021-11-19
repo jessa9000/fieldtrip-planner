0e11c83057fa0a4526825f574a5c2d6eb0ce0b1e
jessa9000 <ealickj@oregonstate.edu>
2021-11-18 20:05:27 -0500

    Removed sample list of dictionaries from starter app

--- app.py
+++ app.py
@@ -6,40 +6,13 @@ import database.db_connector as db
 
 app = Flask(__name__)
 
-people_from_app_py = [
-{
-    "name": "Thomas",
-    "age": 33,
-    "location": "New Mexico",
-    "favorite_color": "Blue"
-},
-{
-    "name": "Gregory",
-    "age": 41,
-    "location": "Texas",
-    "favorite_color": "Red"
-},
-{
-    "name": "Vincent",
-    "age": 27,
-    "location": "Ohio",
-    "favorite_color": "Green"
-},
-{
-    "name": "Alexander",
-    "age": 29,
-    "location": "Florida",
-    "favorite_color": "Orange"
-}
-]
-
 db_connection = db.connect_to_database()
 
 # Routes 
 
-@app.route('/')
-def root():
-    return render_template("main.j2", people=people_from_app_py)
+# @app.route('/')
+# def root():
+#     return render_template("main.j2", people=people_from_app_py)
 
 
 @app.route('/Students')
