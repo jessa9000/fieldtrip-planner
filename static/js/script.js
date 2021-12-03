8df1cc3988763630283267c204cf83932b900f3e
Jessica Ealick <ealickj@flip3.engr.oregonstate.edu>
2021-12-03 13:11:43 -0800

    Added and debugged script to alert short phone num

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,13 @@
 // This is the file where we will keep client-side scripts. //
 
+function checkTelNo(){
+    let telephone = document.getElementById('phone');
+    let telValue = telephone.value;
+    console.log(telValue);
+    if(telValue.length < 10) {
+        event.preventDefault(); 
+        alert("Telephone numbers must be at least 10 digits long.");
+        return false;
+    }
+    return true;
+}
