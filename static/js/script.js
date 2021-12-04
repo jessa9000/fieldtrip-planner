5eed0505285e0ac41427265c91940e54d10d05cb
Jessica Ealick <ealickj@flip3.engr.oregonstate.edu>
2021-12-04 15:03:18 -0800

    Wk in progress: Allergen ID of Allergies sortable

--- static/js/script.js
+++ static/js/script.js
@@ -11,3 +11,56 @@ function checkTelNo(){
     }
     return true;
 }
+
+//Citation: the following sort function is adapted from https://www.w3schools.com/howto/howto_js_sort_table.asp//
+
+function sortNumColumn(){
+    table = document.getElementById("allergiesTable");
+    headerRowCells = table.rows[0].cells;
+    // for (let headerCell of headerRowCells) {
+    //     headerCell.onclick = function(){        //listener on each header cell kicks off a callback function
+    //         let tBody = table.tBodies[0];       //extract the body from what is aprntly an array of table bodies
+    //         let rows = tBody.rows;              //focus on rows of the body
+    //         for (let row of rows) {             //now we're iterating through each row
+    //             let cells = row.cells;
+                
+    //         }
+    //     }
+    // }
+    headerRowCells[3].onclick = function() {
+        let switching = true;
+        let switchCount = 0;
+        var shouldSwitch;
+        var dir = "asc";
+        while (switching) {
+            switching = false;
+            rows = table.rows;
+            for (i=1; i < (rows.length - 1); i++){
+                shouldSwitch = false;
+                x = rows[i].getElementsByTagName("TD")[3];
+                y = rows[i + 1].getElementsByTagName("TD")[3];
+                if (dir == "asc"){
+                    if (Number(x.innerHTML) > Number(y.innerHTML)){
+                        shouldSwitch = true;
+                        break;
+                    } 
+                } else if (dir == "desc") {
+                    if (Number(x.innerHTML) < Number(y.innerHTML)){
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            if (shouldSwitch) {
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                switchCount ++;
+            } else {
+                if (switchCount == 0 && dir == "asc") {
+                    dir = "desc";
+                    switching = true;
+                }
+            }
+        }
+    }
+}
